d4d15d2f3b4e2002860e8225d75708e8c52a8b06
Elena Tsoy <ltsoy1120@gmail.com>
2024-06-07 15:39:42 +0500

    added messageHandler

--- src/frame.ts
+++ src/frame.ts
@@ -20,5 +20,42 @@ export const frame = {
       frame.style.display = "block"
       win.postMessage(JSON.stringify(data), "*")
     }
+
+    // Функция-обработчик события message
+    const messageHandler = function (event: MessageEvent<string>) {
+      const data = event.data
+      console.log("message from c2c", data)
+      if (data === "FAIL") {
+        if (frame) {
+          ;(frame as HTMLIFrameElement).style.display = "none" // Закрываем фрейм
+        }
+        return
+      }
+      if (data === "SUCCESS") {
+        if (frame) {
+          ;(frame as HTMLIFrameElement).style.display = "none" // Закрываем фрейм
+        }
+        return
+      }
+      // if (
+      //   data &&
+      //   typeof data === "object" &&
+      //   (data.status === "IDENTIFIED" ||
+      //     data.status === "NOT_IDENTIFIED" ||
+      //     data.status === "BLOCKED") &&
+      //   state.userId
+      // ) {
+      //   if (data.subStatus && data.subStatus !== "UNFINISHED") {
+      //     // Вызываем пользовательскую функцию для получения персональных данных
+      //     getPersonalData(state.userId)
+      //   }
+
+      //   frame.style.display = "none" // Закрываем фрейм
+
+      //   window.removeEventListener("message", messageHandler)
+      // }
+    }
+
+    window.addEventListener("message", messageHandler, false)
   }
 }
